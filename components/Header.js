a589dfe7322882028bfdefd498237c3313f9dc85
Henry Beveridge <henrydbeveridge@gmail.com>
2022-08-10 21:13:58 -0700

    Create a basic header

--- components/Header.js
+++ components/Header.js
@@ -1,4 +1,21 @@
+import { faCoffee } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Link from "next/link";
+
 
 export default function Header() {
-   return (<h1></h1>)
+
+
+   return (<header>
+      <ul className="left">
+         <li className="home-link-wrapper">
+            <Link href='/' className="home-link">
+               <><FontAwesomeIcon icon={faCoffee} /> <p id="home-link-text">Tea Blog</p></>
+            </Link>
+            </li>
+         <li>Article List</li>
+         <li>Random Article</li>
+      </ul>
+   </header>)
 }
+
--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,14 @@
 import '../styles/normalize.css'
 import '../styles/globals.css'
 import Header from '../components/Header'
+import Head from 'next/head'
+
 
 function MyApp({ Component, pageProps }) {
   return <>
+      <Head>
+        <title>Tea Blog</title> 
+      </Head>      
       <Header />
       <Component {...pageProps} />
     </>
